1233f672014dfb9089ad10040f1e4a9eda3fd7ac
CDRafaFan <113998036+CDRafaFan@users.noreply.github.com>
2022-10-28 16:20:53 +0530

    Create cat.c

---
+++ cat.c
@@ -0,0 +1,136 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <libgen.h>
+int main(int argc,char* argv[])
+{
+    int len=0;
+    int cap=16;
+    char **tok=malloc(16*sizeof(char *));
+    char *delim=" \t\r\n";
+    char *token=strtok(argv[1],delim);
+    while(token!=NULL)
+    {
+        tok[len]=token;
+        len++;
+        if(len>=cap)
+        {
+            cap=(int)(cap*1.5);
+            tok=realloc(tok,cap*sizeof(char*));
+        }
+        token=strtok(NULL,delim);
+    }
+    tok[len]=NULL;
+    if(strcmp(tok[0],"-n")!=0 && strcmp(tok[0],"-E")!=0)
+    {
+        for(int i=0;tok[i]!=NULL;i++)
+        {
+            FILE *pointer;
+            char character;
+            pointer=fopen(tok[i],"r");
+            if(pointer==NULL)
+            {
+                printf("cat: %s: No such file or directory\n",tok[i]);
+            }
+            else
+            {
+                character=fgetc(pointer);
+                while(character!=EOF)
+                {
+                    printf("%c",character);
+                    character=fgetc(pointer);
+                }
+            }
+            fclose(pointer);
+        }
+    }
+    else
+    if(strcmp(tok[0],"-n")==0)
+    {
+        int count=1;
+        for(int i=1;tok[i]!=NULL;i++)
+        {
+            FILE *pointer;
+            char character;
+            pointer=fopen(tok[i],"r");
+            if(pointer==NULL)
+            {
+                printf("cat: %s: No such file or directory\n",tok[i]);
+            }
+            else
+            {
+                character=fgetc(pointer);int flag=0;
+                printf("%d) ",count++);
+                while(character!=EOF)
+                {
+                    if(flag==0)
+                    {
+                        printf("%c",character);
+                    }
+                    else
+                    if(flag==1)
+                    {
+                        printf("%c",character);
+                        flag=2;
+                    }
+                    else
+                    if(flag==2)
+                    {
+                        printf("%d) %c",count++,character);
+                        flag=0;
+                    }
+                    character=fgetc(pointer);
+                    if(character=='\n')
+                    flag=1;
+                }
+            }
+            fclose(pointer);
+        }
+    }
+    else
+    if(strcmp(tok[0],"-E")==0)
+    {
+        int count=0;
+        for(int i=1;tok[i]!=NULL;i++)
+        {
+            count++;
+        }
+        for(int i=1;tok[i]!=NULL;i++)
+        {
+            FILE *pointer;
+            char character;
+            pointer=fopen(tok[i],"r");
+            if(pointer==NULL)
+            {
+                printf("cat: %s: No such file or directory\n",tok[i]);
+            }
+            else
+            {
+                character=fgetc(pointer);int flag=0;
+                while(character!=EOF)
+                {
+                    if(flag==0)
+                    {
+                        printf("%c",character);
+                    }
+                    else
+                    if(flag==1)
+                    {
+                        printf("$");
+                        printf("%c",character);
+                        flag=0;
+                    }
+                    character=fgetc(pointer);
+                    if(character=='\n')
+                    flag=1;
+                }
+                if(character==EOF && i==(count))
+                printf("$");
+            }
+            fclose(pointer);
+        }
+    }
+}
